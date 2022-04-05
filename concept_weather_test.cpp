74809b10578786af4a6c544e24361e79841b6bab
aabuelazm <abdot525@gmail.com>
2022-04-04 22:18:45 -0700

    kill this pls

--- concept_weather_test.cpp
+++ concept_weather_test.cpp
@@ -3,13 +3,13 @@ using namespace concept_weather;
 using namespace std;
 
 int main() {
-  auto *weather_data = new WeatherData;
-
-  auto *current_display = new CurrentConditionsDisplay(weather_data);
-  auto *statistic_display = new StatisticsDisplay(weather_data);
-  auto *forecast_display = new ForecastDisplay(weather_data);
-
-  weather_data->set_measurements(80, 65, 30.4);
-  weather_data->set_measurements(82, 70, 29.2);
-  weather_data->set_measurements(78, 90, 29.2);
+  // auto *weather_data = new WeatherData;
+  //
+  // auto *current_display = new CurrentConditionsDisplay(weather_data);
+  // auto *statistic_display = new StatisticsDisplay(weather_data);
+  // auto *forecast_display = new ForecastDisplay(weather_data);
+  //
+  // weather_data->set_measurements(80, 65, 30.4);
+  // weather_data->set_measurements(82, 70, 29.2);
+  // weather_data->set_measurements(78, 90, 29.2);
 }
