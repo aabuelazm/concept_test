befcf42aa4e3d30dfb308dae5fe4c227ff1c5c75
aabuelazm <abdot525@gmail.com>
2022-04-04 21:39:49 -0700

    Let us go part 2!

--- concept_weather_test.cpp
+++ concept_weather_test.cpp
@@ -3,13 +3,13 @@ using namespace concept_weather;
 using namespace std;
 
 int main() {
-  WeatherData *weather_data = new WeatherData;
+  WeatherData weather_data;
 
-  CurrentConditionsDisplay current_display(weather_data);
-  StatisticsDisplay static_display(weather_data);
-  ForecastDisplay forecast_display(weather_data);
+  CurrentConditionsDisplay current_display(&weather_data);
+  StatisticsDisplay static_display(&weather_data);
+  ForecastDisplay forecast_display(&weather_data);
 
-  weather_data->set_measurements(80, 65, 30.4);
-  weather_data->set_measurements(82, 70, 29.2);
-  weather_data->set_measurements(78, 90, 29.2);
+  weather_data.set_measurements(80, 65, 30.4);
+  weather_data.set_measurements(82, 70, 29.2);
+  weather_data.set_measurements(78, 90, 29.2);
 }
