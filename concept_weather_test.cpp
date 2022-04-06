ae578aabf4fa22c414bed2efa51dbb3d51bac2b4
aabuelazm <abdot525@gmail.com>
2022-04-06 09:54:02 -0700

    why :(

--- concept_weather_test.cpp
+++ concept_weather_test.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
   auto *weather_data = new WeatherData;
 
-  auto *current_display = new CurrentConditionsDisplay(weather_data);
-  auto *statistic_display = new StatisticsDisplay(weather_data);
-  auto *forecast_display = new ForecastDisplay(weather_data);
+  new CurrentConditionsDisplay(weather_data);
+  new StatisticsDisplay(weather_data);
+  new ForecastDisplay(weather_data);
 
   weather_data->set_measurements(80, 65, 30.4);
   weather_data->set_measurements(82, 70, 29.2);
